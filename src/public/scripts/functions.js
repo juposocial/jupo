7224861f491fa5f2be1b4fd4149212b9d9deef1c
Andy Pham <andy@jupo.com>
2013-05-06 15:18:22 +0700

    Bugfix: prevent chatbox from showing duplicate messages

--- src/public/scripts/functions.js
+++ src/public/scripts/functions.js
@@ -1430,22 +1430,25 @@ function stream() {
       var msg_ts = msg.data('ts');
       var sender_id = msg.attr('data-sender-id');
       
-      if (msg_ts - last_msg.data('ts') < 120 && last_msg.attr('data-sender-id') == sender_id) {
-        if (last_msg.attr('data-msg-ids').indexOf(msg_id) == -1) {
-          var content = $('.content', msg).html();
-          $('.content', last_msg).html($('.content', last_msg).html() + '<br>' + content);
-          $(last_msg).data('ts', msg_ts);
-          $(last_msg).attr('data-msg-ids', $(last_msg).attr('data-msg-ids') + ',' + msg_id);
+      if (last_msg.attr('data-msg-ids').indexOf(msg_id) == -1) {
+        if (msg_ts - last_msg.data('ts') < 120 && last_msg.attr('data-sender-id') == sender_id) {
+          if (last_msg.attr('data-msg-ids').indexOf(msg_id) == -1) {
+            var content = $('.content', msg).html();
+            $('.content', last_msg).html($('.content', last_msg).html() + '<br>' + content);
+            $(last_msg).data('ts', msg_ts);
+            $(last_msg).attr('data-msg-ids', $(last_msg).attr('data-msg-ids') + ',' + msg_id);
+          }
+        } else {
+          $('.messages', boxchat).append(_msg);
         }
-      } else {
-        $('.messages', boxchat).append(_msg);
+        
+        setTimeout(function() {
+          $('.messages', boxchat).scrollTop(99999);
+        }, 10)  
       }
       
       $('div.status', boxchat).fadeOut('fast');
       
-      setTimeout(function() {
-        $('.messages', boxchat).scrollTop(99999);
-      }, 10)  
 
     }
     
