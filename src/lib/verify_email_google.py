eb00cfa73a9be9dbea1239c4e50c7daba0828d66
Nguyen Huu Thong <simzen85@gmail.com>
2013-08-28 09:46:26 +0700

    Redesign homepage - update lib to validate email #4

--- src/lib/verify_email_google.py
+++ src/lib/verify_email_google.py
@@ -4,16 +4,17 @@ from validate_email import validate_email
 
 def check_google_email(email):
   hostname = email[email.find('@')+1:]
-  mx_hosts = DNS.mxlookup(hostname)
-  check_google_mail = False
+  
+  try:
+    mx_hosts = DNS.mxlookup(hostname)
+  except:
+    return False
+  
   for mx in mx_hosts:
     _, host_server = mx
     if 'google' in str(host_server).lower() and 'aspmx' in str(host_server).lower():
-      check_google_mail = True
-  
-  if check_google_mail:
-    return validate_email(email, verify=True)
-  
-  else:
-    return False
-  
+      return True
+    
+  return False
+
+print check_google_email('hungdv@joomsolutions.com')
