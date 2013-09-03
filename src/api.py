2da41bd2d5d59ef3a7853d10e7a1d536ddbb532d
Andy Pham <andy@jupo.com>
2013-09-03 16:25:50 +0700

    Fix coding style

--- src/api.py
+++ src/api.py
@@ -2151,8 +2151,11 @@ def get_member_email_addresses(db_name=None):
     db_name = get_database_name()
   db = DATABASE[db_name]
 
-  # find all email addresses in network, watchout for group (same table owner, got no email field)
-  member_email_addresses = [i['email'] for i in db.owner.find({'email': {'$ne': None}}, {'email': True})]
+  # find all email addresses in network, watchout for group 
+  # (same table owner, got no email field)
+  member_email_addresses = [i['email'] \
+                            for i in db.owner.find({'email': {'$ne': None}}, 
+                                                   {'email': True})]
 
   return member_email_addresses
 
@@ -2161,10 +2164,15 @@ def get_invited_addresses(db_name=None, user_id=None):
     db_name = get_database_name()
   db = DATABASE[db_name]
 
-  # find all email addresses that *NOT YET SIGNED UP BUT ALREADY SENT INVITATION* in network, watchout for group (same table owner, got no email field)
-  # invited_address = owner that got name = NONE (can't use password since new signup workflow doesn't require password)
-  invited_addresses = [i['email'] for i in db.owner.find({'email': {'$ne': None}, 'name': None, 'ref': user_id} , {'email': True})]
-  # print "DEBUG - in get_invited_addresses - invited_addresses = " + str(invited_addresses)
+  # find all email addresses that *NOT YET SIGNED UP BUT ALREADY SENT INVITATION* 
+  # in network, watchout for group (same table owner, got no email field)
+  # invited_address = owner that got name = NONE 
+  # (can't use password since new signup workflow doesn't require password)
+  invited_addresses = [i['email'] \
+                       for i in db.owner.find({'email': {'$ne': None}, 
+                                               'name': None, 
+                                               'ref': user_id}, 
+                                              {'email': True})]
 
   return invited_addresses
 
